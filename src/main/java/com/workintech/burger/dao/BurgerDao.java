cacf7297383eed649f4673dbb4cf1d8010b024c4
Gülşen Satıcı <125642521+gulsensatici@users.noreply.github.com>
2023-08-29 00:27:48 +0300

    görev 3.2 kaldı

---
+++ src/main/java/com/workintech/burger/dao/BurgerDao.java
@@ -0,0 +1,26 @@
+package com.workintech.burger.dao;
+
+import com.workintech.burger.entity.BreadType;
+import com.workintech.burger.entity.Burger;
+
+import java.util.List;
+
+public interface BurgerDao {
+    Burger save(Burger burger);
+
+    List<Burger> finAll();
+
+    Burger findById(int id);
+
+    List<Burger> findByPrice(double price);
+
+    List<Burger> findByBreadType(String breadType);
+
+    List<Burger> findByContent(String content);
+
+    Burger update(Burger burger);
+
+    void delete(Burger burger);
+
+    List<Burger> findAll();
+}
